83b1e7ae6d20d3ca88d26c45265c4fae4d512efc
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-03 10:18:05 -0500

    feat: added Base and UserResponse

---
+++ ReadOwoBackend/ReadOwo/Domain/Services/Communication/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using ReadOwoBackend.ReadOwo.Domain.Models;
+using ReadOwoBackend.Shared.Domain.Services.Communication;
+
+namespace ReadOwoBackend.ReadOwo.Domain.Services.Communication;
+
+public class UserResponse: BaseResponse<User>
+{
+    public UserResponse(string message) : base(message)
+    {
+    }
+
+    public UserResponse(User resource) : base(resource)
+    {
+    }
+}
