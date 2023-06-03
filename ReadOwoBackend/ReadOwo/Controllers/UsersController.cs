c7f4b4b9afa0223af78fc6097f739720f099733f
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-03 10:58:23 -0500

    feat: added UserController

---
+++ ReadOwoBackend/ReadOwo/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ReadOwoBackend.ReadOwo.Domain.Models;
+using ReadOwoBackend.ReadOwo.Resources;
+using ReadOwoBackend.ReadOwo.Services;
+
+namespace ReadOwoBackend.ReadOwo.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<UserResource>> GetAllAsync()
+    {
+        var users = await _userService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<User>, IEnumerable<UserResource>>(users);
+
+        return resources;
+    } 
+}
