31f90292b799816674721c8380cb325528a639d7
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-03 15:49:03 -0500

    feat: added Program Configuration

--- ReadOwoBackend/Program.cs
+++ ReadOwoBackend/Program.cs
@@ -1,13 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using ReadOwoBackend.ReadOwo.Domain.Repositories;
+using ReadOwoBackend.ReadOwo.Domain.Services;
+using ReadOwoBackend.ReadOwo.Mapping;
+using ReadOwoBackend.ReadOwo.Persistence.Repositories;
+using ReadOwoBackend.ReadOwo.Services;
+using ReadOwoBackend.Shared.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+// Add lowercase routes
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Dependency Injection Configuration
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+// AutoMapper COnfiguration
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile),
+    typeof(ResourceToModelProfile));
+
 var app = builder.Build();
+
+// Validation for ensuring Database Objects are created
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +51,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
