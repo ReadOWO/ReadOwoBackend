da2c7de71d17c5afd016926bb397b7f74d97b3bd
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-03 14:52:50 -0500

    feat: add AppDbContext

---
+++ ReadOwoBackend/ReadOwo/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReadOwoBackend.ReadOwo.Domain.Models;
+
+namespace ReadOwoBackend.ReadOwo.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task AddAsync(User category);
+    Task<User> FindByIdAsync(int id);
+    void Update(User category);
+    void Remove(User category);
+}
---
+++ ReadOwoBackend/ReadOwo/Persistence/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReadOwoBackend.Shared.Extensions;
+using ReadOwoBackend.ReadOwo.Domain.Models;
+
+namespace ReadOwoBackend.ReadOwo.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<User>().ToTable("Users");
+        modelBuilder.Entity<User>().HasKey(p => p.Id);
+        modelBuilder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        modelBuilder.Entity<User>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+        modelBuilder.Entity<User>().Property(p => p.email).IsRequired().HasMaxLength(240);
+        modelBuilder.Entity<User>().Property(p => p.password).IsRequired().HasMaxLength(240);
+        
+        
+        modelBuilder.UseSnakeCaseNamingConvention();
+    }
+}
