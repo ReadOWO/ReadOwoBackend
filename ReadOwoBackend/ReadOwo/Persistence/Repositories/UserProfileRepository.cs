ce9d5ccd10ebeaa050b78197072f8d91b4a2b82f
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-07 10:54:11 -0500

    added UserProfileRepository

--- ReadOwoBackend/ReadOwo/Persistence/Repositories/UserProfileRepository.cs
+++ ReadOwoBackend/ReadOwo/Persistence/Repositories/UserProfileRepository.cs
@@ -1,6 +1,58 @@
-﻿namespace ReadOwoBackend.ReadOwo.Persistence.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using ReadOwoBackend.ReadOwo.Domain.Models;
+using ReadOwoBackend.ReadOwo.Domain.Repositories;
+using ReadOwoBackend.Shared.Persistence.Contexts;
+using ReadOwoBackend.Shared.Persistence.Repositories;
 
-public class UserProfileRepository
+namespace ReadOwoBackend.ReadOwo.Persistence.Repositories;
+
+public class UserProfileRepository : BaseRepository, IUserProfileRepository
 {
-    
+    public UserProfileRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<UserProfile>> ListAsync()
+    {
+        return await _context.Profiles
+            .Include(p => p.User)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(UserProfile userProfile)
+    {
+        await _context.Profiles.AddAsync(userProfile);
+    }
+
+    public async Task<UserProfile> FindByIdAsync(int userProfileId)
+    {
+        return await _context.Profiles
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(p => p.Id == userProfileId);
+    }
+
+    public async Task<UserProfile> FindByNameAsync(string name)
+    {
+        return await _context.Profiles
+            .Include(p => p.User)
+            .FirstOrDefaultAsync(p => p.Name== name);
+    }
+
+    public async Task<IEnumerable<UserProfile>> FindByUserIdAsync(int userId)
+    {
+        return await _context.Profiles
+            .Where(p => p.UserId == userId)
+            .Include(p => p.User)
+            .ToListAsync();
+    }
+
+    public void Update(UserProfile userProfile)
+    {
+        _context.Profiles.Update(userProfile);
+    }
+
+    public void Remove(UserProfile userProfile)
+    {
+        _context.Profiles.Remove(userProfile);
+    }
 }
