af4b29d88b15e9c38fdac801e4487b8a2f97e309
Stevens <u20201e735@upc.edu.pe>
2023-06-07 15:57:54 -0500

    feat: add language response and language service interface

---
+++ ReadOwoBackend/Publishing/Domain/Services/Communication/LanguageResponse.cs
@@ -0,0 +1,15 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Shared.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+public class LanguageResponse : BaseResponse<Language>
+{
+    public LanguageResponse(string message) : base(message)
+    {
+    }
+
+    public LanguageResponse(Language resource) : base(resource)
+    {
+    }
+}
---
+++ ReadOwoBackend/Publishing/Domain/Services/ILanguageService.cs
@@ -0,0 +1,13 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services;
+
+public interface ILanguageService
+{
+    Task<IEnumerable<Language>> ListAsync();
+    Task<IEnumerable<Language>> ListByLanguageIdAsync(int languageId);
+    Task<LanguageResponse> SaveAsync(Language language);
+    Task<LanguageResponse> UpdateAsync(int languageId);
+    Task<LanguageResponse> DeleteAsync(int languageId);
+}
