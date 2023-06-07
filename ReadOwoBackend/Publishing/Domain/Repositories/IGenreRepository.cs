22b000fb183163ecd61be2c6e0ffbaeba4cb1c14
Stevens <u20201e735@upc.edu.pe>
2023-06-07 12:27:42 -0500

    feat: add genre repository interface

---
+++ ReadOwoBackend/Publishing/Domain/Repositories/IGenreRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+
+namespace ReadOwoBackend.Publishing.Domain.Repositories;
+
+public interface IGenreRepository
+{
+    Task<IEnumerable<Genre>> ListAsync();
+    Task AddAsync(Genre genre);
+    Task<Genre> FindByIdAsync(int id);
+    void Update(Genre genre);
+    void Remove(Genre genre);
+}
