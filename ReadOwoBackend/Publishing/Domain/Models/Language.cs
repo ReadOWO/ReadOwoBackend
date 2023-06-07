ce684406752b763244295ddf683067b53ba0d572
Stevens <u20201e735@upc.edu.pe>
2023-06-07 15:32:37 -0500

    feat: add language entity

---
+++ ReadOwoBackend/Publishing/Domain/Models/Language.cs
@@ -0,0 +1,8 @@
+﻿namespace ReadOwoBackend.Publishing.Domain.Models;
+
+public class Language
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Abbreviation { get; set; }
+}
