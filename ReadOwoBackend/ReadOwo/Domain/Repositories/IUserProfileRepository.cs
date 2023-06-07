83cc6f33c439a5029d3d6ebc142975db51541139
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-07 09:54:54 -0500

    added IUserProfileRepository and SaveUserProfileResource

---
+++ ReadOwoBackend/ReadOwo/Domain/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReadOwoBackend.ReadOwo.Domain.Models;
+
+namespace ReadOwoBackend.ReadOwo.Domain.Repositories;
+
+public interface IUserProfileRepository
+{
+    Task<IEnumerable<UserProfile>> ListAsync();
+    Task AddAsync(UserProfile userProfile);
+    Task<UserProfile> FindByIdAsync(int userProfileId);
+    Task<UserProfile> FindByNameAsync(string name);
+    Task<IEnumerable<UserProfile>> FindByUserIdAsync(int userId);
+    void Update(UserProfile userProfile);
+    void Remove(UserProfile userProfile);
+}
