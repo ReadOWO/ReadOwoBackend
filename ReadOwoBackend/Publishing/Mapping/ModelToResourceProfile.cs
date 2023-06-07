c518ed7ec9bc18eae4588348ff3ca0b578493c78
Stevens <u20201e735@upc.edu.pe>
2023-06-07 13:03:24 -0500

    feat: add model to resource and resource to model for publishing

---
+++ ReadOwoBackend/Publishing/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Resources;
+
+namespace ReadOwoBackend.Publishing.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<Genre, GenreResource>();
+    }
+}
---
+++ ReadOwoBackend/Publishing/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿
+using AutoMapper;
+using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Resources;
+
+namespace ReadOwoBackend.Publishing.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<SaveGenreResource, Genre>();
+    }
+}
