cb28fcb6050bd59f52e67724d38ad3ad66ec8c3b
Stevens <u20201e735@upc.edu.pe>
2023-06-07 12:29:14 -0500

    feat: add genre response

---
+++ ReadOwoBackend/Publishing/Domain/Services/Communication/GenreResponse.cs
@@ -0,0 +1,15 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Shared.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+public class GenreResponse: BaseResponse<Genre>
+{
+    public GenreResponse(string message) : base(message)
+    {
+    }
+
+    public GenreResponse(Genre resource) : base(resource)
+    {
+    }
+}
