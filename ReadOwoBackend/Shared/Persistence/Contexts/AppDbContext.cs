4f068bf85996e0e66750d3d13121d8c19fc5b157
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-07 10:07:51 -0500

    Update AppDbContext

--- ReadOwoBackend/Shared/Persistence/Contexts/AppDbContext.cs
+++ ReadOwoBackend/Shared/Persistence/Contexts/AppDbContext.cs
@@ -6,12 +6,11 @@ namespace ReadOwoBackend.Shared.Persistence.Contexts;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> Profiles { get; set; } 
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
-
-    public DbSet<User> Users { get; set; }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
@@ -22,8 +21,17 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<User>().Property(p => p.Name).IsRequired().HasMaxLength(30);
         modelBuilder.Entity<User>().Property(p => p.email).IsRequired().HasMaxLength(240);
         modelBuilder.Entity<User>().Property(p => p.password).IsRequired().HasMaxLength(240);
+
+        modelBuilder.Entity<User>()
+            .HasMany(p => p.Profiles)
+            .WithOne(p => p.User)
+            .HasForeignKey(p => p.UserId);
         
-        
+        modelBuilder.Entity<UserProfile>().ToTable("Profiles");
+        modelBuilder.Entity<UserProfile>().HasKey(p => p.Id);
+        modelBuilder.Entity<UserProfile>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        modelBuilder.Entity<UserProfile>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+
         modelBuilder.UseSnakeCaseNamingConvention();
     }
 }
