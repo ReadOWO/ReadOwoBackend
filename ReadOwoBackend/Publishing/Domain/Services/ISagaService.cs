99c09a2bb7736f1332c261521a4f0ecbc8fd9e3b
jhuan <j.huanca4141@gmail.com>
2023-06-08 11:25:23 -0500

    feat: add saga and saga status interface service

---
+++ ReadOwoBackend/Publishing/Domain/Services/ISagaService.cs
@@ -0,0 +1,13 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services;
+
+public interface ISagaService
+{
+    Task<IEnumerable<Saga>> ListAsync();
+    Task<SagaResponse> FindByIdAsync(int sagaId);
+    Task<SagaResponse> SaveAsync(Saga saga);
+    Task<SagaResponse> UpdateAsync(int sagaId, Saga saga);
+    Task<SagaResponse> DeleteAsync(int sagaId);
+}
---
+++ ReadOwoBackend/Publishing/Domain/Services/ISagaStatusService.cs
@@ -0,0 +1,13 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services;
+
+public interface ISagaStatusService
+{
+    Task<IEnumerable<SagaStatus>> ListAsync();
+    Task<SagaStatusResponse> FindByIdAsync(int sagaStatusId);
+    Task<SagaStatusResponse> SaveAsync(SagaStatus sagaStatus);
+    Task<SagaStatusResponse> UpdateAsync(int sagaStatusId, SagaStatus sagaStatus);
+    Task<SagaStatusResponse> DeleteAsync(int sagaStatusId);
+}
