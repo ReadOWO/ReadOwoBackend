63cdc766e4e9410cecac9ebaa9784ccab1516be2
jhuan <j.huanca4141@gmail.com>
2023-06-08 11:41:51 -0500

    feat: add save saga and saga status resource

---
+++ ReadOwoBackend/Publishing/Resources/SaveSagaResource.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadOwoBackend.Publishing.Resources;
+
+public class SaveSagaResource
+{
+    public string Title { get; set; }
+    
+    public string Synopsis { get; set; }
+
+    public int SagaStatusId { get; set; }
+}
