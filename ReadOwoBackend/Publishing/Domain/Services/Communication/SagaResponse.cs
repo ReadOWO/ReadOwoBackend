78156e7dd3ce360a151ccfd1eed809d9ac5020ad
jhuan <j.huanca4141@gmail.com>
2023-06-08 11:23:21 -0500

    feat: add saga and saga status response

---
+++ ReadOwoBackend/Publishing/Domain/Services/Communication/SagaResponse.cs
@@ -0,0 +1,15 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Shared.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+public class SagaResponse: BaseResponse<Saga>
+{
+    public SagaResponse(string message) : base(message)
+    {
+    }
+
+    public SagaResponse(Saga resource) : base(resource)
+    {
+    }
+}
