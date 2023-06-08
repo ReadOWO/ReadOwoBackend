48ecf2d051d57cd299ca78fb967a3fdc1845a9ec
jhuan <j.huanca4141@gmail.com>
2023-06-08 11:12:26 -0500

    feat: add saga and saga status entity

---
+++ ReadOwoBackend/Publishing/Domain/Models/SagaStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace ReadOwoBackend.Publishing.Domain.Models;
+
+public class SagaStatus
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    
+    // Relationships
+    
+    public IList<Saga> Sagas { get; set; } = new List<Saga>();
+}
