1b4c2734e6b87f7b29911c8a52a0ea9e3e4140bb
jhuan <j.huanca4141@gmail.com>
2023-06-08 11:42:21 -0500

    feat: add saga and saga status service

---
+++ ReadOwoBackend/Publishing/Services/SagaService.cs
@@ -0,0 +1,127 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Repositories;
+using ReadOwoBackend.Publishing.Domain.Services;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
+using ReadOwoBackend.ReadOwo.Domain.Repositories;
+
+namespace ReadOwoBackend.Publishing.Services;
+
+public class SagaService : ISagaService
+{
+    private readonly ISagaRepository _sagaRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ISagaStatusRepository _sagaStatusRepository;
+
+
+    public SagaService(ISagaRepository sagaRepository, IUnitOfWork unitOfWork, ISagaStatusRepository sagaStatusRepository)
+    {
+        _sagaRepository = sagaRepository;
+        _unitOfWork = unitOfWork;
+        _sagaStatusRepository = sagaStatusRepository;
+    }
+    
+    public async Task<IEnumerable<Saga>> ListAsync()
+    {
+        return await _sagaRepository.ListAsync();
+    }
+    
+    public async Task<SagaResponse> FindByIdAsync(int sagaId)
+    {
+        var saga = await _sagaRepository.FindByIdAsync(sagaId);
+        
+        if (saga == null)
+            return new SagaResponse("Saga not found");
+        
+        return new SagaResponse(saga);
+    }
+
+    public async Task<SagaResponse> SaveAsync(Saga saga)
+    {
+        // Validate CategoryId
+
+        var existingSagaStatus = await _sagaStatusRepository.FindByIdAsync(saga.SagaStatusId);
+
+        if (existingSagaStatus == null)
+            return new SagaResponse("Invalid Saga Status");
+
+        try
+        {
+            // Add Tutorial
+            await _sagaRepository.AddAsync(saga);
+            
+            // Complete Transaction
+            await _unitOfWork.CompleteAsync();
+            
+            // Return response
+            return new SagaResponse(saga);
+
+        }
+        catch (Exception e)
+        {
+            // Error Handling
+            return new SagaResponse($"An error occurred while saving the tutorial: {e.Message}");
+        }
+
+        
+    }
+
+    public async Task<SagaResponse> UpdateAsync(int sagaId, Saga saga)
+    {
+        // Validate Saga
+        var existingSaga = await _sagaRepository.FindByIdAsync(sagaId);
+
+        if (existingSaga == null)
+            return new SagaResponse("Saga not found.");
+
+        // Validate SagaStatusId
+
+        var existingSagaStatus = await _sagaStatusRepository.FindByIdAsync(saga.SagaStatusId);
+
+        if (existingSagaStatus == null)
+            return new SagaResponse("Invalid Saga Status");
+
+        // Modify Fields
+        existingSaga.Title = saga.Title;
+        existingSaga.Synopsis = saga.Synopsis;
+
+        try
+        {
+            _sagaRepository.Update(existingSaga);
+            await _unitOfWork.CompleteAsync();
+
+            return new SagaResponse(existingSaga);
+            
+        }
+        catch (Exception e)
+        {
+            // Error Handling
+            return new SagaResponse($"An error occurred while updating the tutorial: {e.Message}");
+        }
+
+    }
+
+    public async Task<SagaResponse> DeleteAsync(int sagaId)
+    {
+        var existingSaga = await _sagaRepository.FindByIdAsync(sagaId);
+        
+        // Validate Tutorial
+
+        if (existingSaga == null)
+            return new SagaResponse("Tutorial not found.");
+        
+        try
+        {
+            _sagaRepository.Remove(existingSaga);
+            await _unitOfWork.CompleteAsync();
+
+            return new SagaResponse(existingSaga);
+            
+        }
+        catch (Exception e)
+        {
+            // Error Handling
+            return new SagaResponse($"An error occurred while deleting the tutorial: {e.Message}");
+        }
+
+    }
+}
---
+++ ReadOwoBackend/Publishing/Services/SagaStatusService.cs
@@ -0,0 +1,105 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Repositories;
+using ReadOwoBackend.Publishing.Domain.Services;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
+using ReadOwoBackend.ReadOwo.Domain.Repositories;
+
+namespace ReadOwoBackend.Publishing.Services;
+
+public class SagaStatusService : ISagaStatusService
+{
+    private readonly ISagaStatusRepository _sagaStatusRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+
+    public SagaStatusService(ISagaStatusRepository sagaStatusRepository, IUnitOfWork unitOfWork)
+    {
+        _sagaStatusRepository = sagaStatusRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IEnumerable<SagaStatus>> ListAsync()
+    {
+        return await _sagaStatusRepository.ListAsync();
+    }
+    
+    public async Task<SagaStatusResponse> FindByIdAsync(int sagaStatusId)
+    {
+        var existingSagaStatus = await _sagaStatusRepository.FindByIdAsync(sagaStatusId);
+        
+        if (existingSagaStatus == null)
+            return new SagaStatusResponse("Saga not found");
+        
+        return new SagaStatusResponse(existingSagaStatus);
+    }
+
+    public async Task<SagaStatusResponse> SaveAsync(SagaStatus sagaStatus)
+    {
+        try
+        {
+            await _sagaStatusRepository.AddAsync(sagaStatus);
+            await _unitOfWork.CompleteAsync();
+            return new SagaStatusResponse(sagaStatus);
+
+        }
+        catch (Exception e)
+        {
+            return new SagaStatusResponse($"An error occurred while saving the tutorial: {e.Message}");
+        }
+
+        
+    }
+
+    public async Task<SagaStatusResponse> UpdateAsync(int sagaStatusId, SagaStatus sagaStatus)
+    {
+        var existingSagaStatus = await _sagaStatusRepository.FindByIdAsync(sagaStatusId);
+        
+        // Validate Saga
+
+        if (existingSagaStatus == null)
+            return new SagaStatusResponse("Saga not found.");
+
+        // Modify Fields
+        existingSagaStatus.Name = sagaStatus.Name;
+
+        try
+        {
+            _sagaStatusRepository.Update(existingSagaStatus);
+            await _unitOfWork.CompleteAsync();
+
+            return new SagaStatusResponse(existingSagaStatus);
+            
+        }
+        catch (Exception e)
+        {
+            // Error Handling
+            return new SagaStatusResponse($"An error occurred while updating the tutorial: {e.Message}");
+        }
+
+    }
+
+    public async Task<SagaStatusResponse> DeleteAsync(int sagaStatusId)
+    {
+        var existingSagaStatus = await _sagaStatusRepository.FindByIdAsync(sagaStatusId);
+        
+        // Validate Tutorial
+
+        if (existingSagaStatus == null)
+            return new SagaStatusResponse("Tutorial not found.");
+        
+        try
+        {
+            _sagaStatusRepository.Remove(existingSagaStatus);
+            await _unitOfWork.CompleteAsync();
+
+            return new SagaStatusResponse(existingSagaStatus);
+            
+        }
+        catch (Exception e)
+        {
+            // Error Handling
+            return new SagaStatusResponse($"An error occurred while deleting the tutorial: {e.Message}");
+        }
+
+    }
+}
