c408d71dd8bc3fdce8a60d69df3a723e4e0fc815
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 07:58:17 -0500

    feat: added book status service interface.

---
+++ ReadOwoBackend/Publishing/Domain/Services/IBookStatusService.cs
@@ -0,0 +1,12 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+
+namespace ReadOwoBackend.Publishing.Domain.Services;
+
+public interface IBookStatusService
+{
+    Task<IEnumerable<BookStatus>> ListAsync();
+    Task<BookStatus> FindByIdAsync(int bookStatusId);
+    Task<BookStatus> SaveAsync(BookStatus bookStatus);
+    Task<BookStatus> UpdateAsync(int bookStatusId, BookStatus bookStatus);
+    Task<BookStatus> DeleteAsync(int bookStatusId);
+}
