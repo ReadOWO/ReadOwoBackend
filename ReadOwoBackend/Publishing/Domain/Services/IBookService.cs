68ed1a65bf07415467f2a75f37a36f81bd480c99
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 10:17:37 -0500

    fix: fixed object type of task.

--- ReadOwoBackend/Publishing/Domain/Services/IBookService.cs
+++ ReadOwoBackend/Publishing/Domain/Services/IBookService.cs
@@ -1,12 +1,13 @@
 ﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
 
 namespace ReadOwoBackend.Publishing.Domain.Services;
 
 public interface IBookService
 {
     Task<IEnumerable<Book>> ListAsync();
-    Task<Book> FindByIdAsync(int bookId);
-    Task<Book> SaveAsync(Book book);
-    Task<Book> UpdateAsync(int bookId, Book book);
-    Task<Book> DeleteAsync(int bookId);
+    Task<BookResponse> FindByIdAsync(int bookId);
+    Task<BookResponse> SaveAsync(Book book);
+    Task<BookResponse> UpdateAsync(int bookId, Book book);
+    Task<BookResponse> DeleteAsync(int bookId);
 }
--- ReadOwoBackend/Publishing/Domain/Services/IBookStatusService.cs
+++ ReadOwoBackend/Publishing/Domain/Services/IBookStatusService.cs
@@ -1,12 +1,13 @@
 ﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Publishing.Domain.Services.Communication;
 
 namespace ReadOwoBackend.Publishing.Domain.Services;
 
 public interface IBookStatusService
 {
     Task<IEnumerable<BookStatus>> ListAsync();
-    Task<BookStatus> FindByIdAsync(int bookStatusId);
-    Task<BookStatus> SaveAsync(BookStatus bookStatus);
-    Task<BookStatus> UpdateAsync(int bookStatusId, BookStatus bookStatus);
-    Task<BookStatus> DeleteAsync(int bookStatusId);
+    Task<BookStatusResponse> FindByIdAsync(int bookStatusId);
+    Task<BookStatusResponse> SaveAsync(BookStatus bookStatus);
+    Task<BookStatusResponse> UpdateAsync(int bookStatusId, BookStatus bookStatus);
+    Task<BookStatusResponse> DeleteAsync(int bookStatusId);
 }
