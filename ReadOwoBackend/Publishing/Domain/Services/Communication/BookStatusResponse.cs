34a3511ff07342340a6b63f86c99cc38d6193235
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 07:54:14 -0500

    feat: added book status response.

---
+++ ReadOwoBackend/Publishing/Domain/Services/Communication/BookStatusResponse.cs
@@ -0,0 +1,11 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+using ReadOwoBackend.Shared.Domain.Services.Communication;
+
+namespace ReadOwoBackend.Publishing.Domain.Services.Communication;
+
+public class BookStatusResponse : BaseResponse<BookStatus>
+{
+    public BookStatusResponse(string message): base(message){}
+    
+    public BookStatusResponse(string resource): base(resource){}
+}
