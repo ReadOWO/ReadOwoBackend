5b79971112ab7d07cf1dc25781bf99850043d75e
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 07:45:01 -0500

    feat: added book interface repository.

---
+++ ReadOwoBackend/Publishing/Domain/Repositories/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+
+namespace ReadOwoBackend.Publishing.Domain.Repositories;
+
+public interface IBookRepository
+{
+    Task<IEnumerable<Book>> ListAsync();
+    Task AddAsync(Book book);
+    Task<Book> FindByIdAsync(int id);
+    void Update(Book book);
+    void Remove(Book book);
+}
