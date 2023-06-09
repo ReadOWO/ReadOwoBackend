df30807b7a2261d6745123d42ac804fe70fa98ca
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 07:41:29 -0500

    feat: added book and book status entity.

---
+++ ReadOwoBackend/Publishing/Domain/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadOwoBackend.Publishing.Domain.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Synopsis { get; set; }
+    public int BookStatusId { get; set; }
+    public BookStatus BookStatus { get; set; } 
+}
---
+++ ReadOwoBackend/Publishing/Domain/Models/BookStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadOwoBackend.Publishing.Domain.Models;
+
+public class BookStatus
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    //Relationships
+    public IList<Book> Books { get; set; } = new List<Book>();
+}
