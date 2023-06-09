cd3737aeab1d0f82cde04eff0ddb2856c04f7b69
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 09:04:12 -0500

    feat: added save book resource.

---
+++ ReadOwoBackend/Publishing/Resources/SaveBookResource.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadOwoBackend.Publishing.Resources;
+
+public class SaveBookResource
+{
+    public string Title { get; set; }
+    
+    public string Synopsis { get; set; }
+    
+    public int BookStatusId { get; set; }
+}
