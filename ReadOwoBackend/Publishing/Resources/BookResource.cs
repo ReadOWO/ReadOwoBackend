8e6085670a2b3f47c553a6a3252585b926b99fda
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 09:00:41 -0500

    feat: added book resource.

---
+++ ReadOwoBackend/Publishing/Resources/BookResource.cs
@@ -0,0 +1,11 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+
+namespace ReadOwoBackend.Publishing.Resources;
+
+public class BookResource
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Synopsis { get; set; }
+    public BookStatusResource BookStatus { get; set; }
+}
