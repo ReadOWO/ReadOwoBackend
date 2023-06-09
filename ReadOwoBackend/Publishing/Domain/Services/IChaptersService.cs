187e0b4a97ec730cd38ff059ba524e09677d5c22
Anderson Velasquez <andersonvelezca@gmail.com>
2023-06-09 18:06:17 -0500

    feat: added Services for chapters

--- ReadOwoBackend/Publishing/Domain/Services/IChaptersService.cs
+++ ReadOwoBackend/Publishing/Domain/Services/IChaptersService.cs
@@ -1,6 +1,12 @@
+using ReadOwoBackend.Publishing.Domain.Models;
+
 namespace ReadOwoBackend.Publishing.Domain.Services;
 
 public interface IChaptersService
 {
-    
+    Task<IEnumerable<Chapters>> ListAsync();
+    Task AddAsync(Chapters chapters);
+    Task<Chapters> FindByIdAsync(int id);
+    void Update(Chapters  chapters);
+    void Remove(Chapters  chapters);
 }
