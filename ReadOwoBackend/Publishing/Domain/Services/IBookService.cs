e42ba67e95c868b19a8fd1c5e8bcbaf32471d06b
Salomón Zegarra <salozm34@gmail.com>
2023-06-09 07:58:10 -0500

    feat: added book service interface.

---
+++ ReadOwoBackend/Publishing/Domain/Services/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using ReadOwoBackend.Publishing.Domain.Models;
+
+namespace ReadOwoBackend.Publishing.Domain.Services;
+
+public interface IBookService
+{
+    Task<IEnumerable<Book>> ListAsync();
+    Task<Book> FindByIdAsync(int bookId);
+    Task<Book> SaveAsync(Book book);
+    Task<Book> UpdateAsync(int bookId, Book book);
+    Task<Book> DeleteAsync(int bookId);
+}
